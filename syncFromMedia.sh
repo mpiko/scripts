f9cd2f45389f077d3d2838325c5f1fe73a0fc55b
Michael Piko <michael@piko.com.au>
2014-04-10 18:22:41 +1000

    need to sudo on the chown command

--- syncFromMedia.sh
+++ syncFromMedia.sh
@@ -10,7 +10,7 @@ cd
 rsync -ave ssh 'media@media-laptop:/home/hts/recordedtv/Oz\ Fish*' /home/lawrie > $LOGFILE 
 rsync -ave ssh 'media@media-laptop:/home/hts/recordedtv/Australian\ Fishing*' /home/lawrie >> $LOGFILE
 
-chown -R michael.public /home/lawrie
+sudo chown -R michael.public /home/lawrie
 
 cleandir $LOGDIR 4
 
