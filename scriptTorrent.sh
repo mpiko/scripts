d8aeb53bec67b47e2c5692c5a2b90082ba4333fc
Michael Piko <michael@piko.com.au>
2014-06-04 12:03:41 +1000

    need to chmod the script

--- scriptTorrent.sh
+++ scriptTorrent.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-
 EPISODE=$1
 ME=$(basename $0)
 
 COMMAND=$(ps ax | grep $EPISODE | grep -v grep |grep -v $ME | cut -f11- -d" "| sed 's/&/\\&/g')
 
-echo CMD = $COMMAND
 echo "$COMMAND" > ~/torrent/${EPISODE}.sh
-echo Is now in ~/torrent/${EPISODE}.sh
+chmod 755 ~/torrent/${EPISODE}.sh
+echo CMD = $COMMAND
+echo "Is now in ~/torrent/${EPISODE}.sh"
