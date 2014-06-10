6d75f54bb0b064b321720fed853f3e7f4b18bc21
Michael Piko <michael@piko.com.au>
2014-06-11 09:56:40 +1000

    minor updates

--- setupNewHost.sh
+++ setupNewHost.sh
@@ -8,11 +8,14 @@ cd $HOME
 setupSSHinto $NEWHOST
 
 # set up the bin utils
-tar zcvf /tmp/bin.tgz bin
-scp /tmp/bin.tgz $NEWHOST:/tmp
-ssh $NEWHOST "tar zxf /tmp/bin.tgz"
+# tar zcvf /tmp/bin.tgz bin
+# scp /tmp/bin.tgz $NEWHOST:/tmp
+# ssh $NEWHOST "tar zxf /tmp/bin.tgz"
 ssh $NEWHOST "[ -d localbin ] || mkdir -p localbin"
-
+ssh $NEWHOST "git clone git://github.com/mpiko/scripts.git"
+ssh $NEWHOST "mv scripts bin"
+scp bin/.secrets $NEWHOST:bin/
+ 
 # set up executable path to include the new bin dir.
 ssh $NEWHOST "echo \"PATH=\\\$PATH:\\\$HOME/localbin\" >> .profile"
 
