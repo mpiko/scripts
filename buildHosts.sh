199eac0b549db2f8171e98d286752c30dbf07b84
Michael Piko <michael@piko.com.au>
2014-06-19 18:01:52 +1000

    initial commit

--- buildHosts.sh
+++ buildHosts.sh
@@ -2,21 +2,26 @@
 
 HOSTS="media@media-laptop pi@rpi ltsp vserver001 vserver002"
 
-cd $HOME/bin
+MYIP=$(ipconfig | grep wlan0 | sed 's/ \+/ /g' | cut -f2 -d" " | sed 's/Addr://')
+
+cat allhosts.master | sed "s/MYIP/$MYIP/" > /tmp/allhosts.txt
+
 
 # me first!
 echo Updating laptop
-cp allhosts.txt /tmp
+#cp allhosts.txt /tmp
 cat /etc/hosts > /tmp/backup_hosts.txt;
 head -3 /etc/hosts > /tmp/newhosts;
 cat /tmp/allhosts.txt >> /tmp/newhosts;
 sudo su -c "cat /tmp/newhosts > /etc/hosts"
 
 
+cd $HOME/bin
+
 for DEST in $HOSTS
 do
   echo Updating $DEST
-  scp allhosts.txt $DEST:/tmp > /dev/null 2>&1
+  scp /tmp/allhosts.txt $DEST:/tmp > /dev/null 2>&1
   if [ $? -eq 0 ]
   then
     ssh $DEST "sudo cat /etc/hosts > /tmp/backup_hosts.txt;
@@ -27,3 +32,4 @@ do
     echo "$DEST is unreachable"
   fi
 done
+
