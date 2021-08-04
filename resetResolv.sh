a7b134d6c783ccff7d8428e38b665e3762a16a43
mpiko <michael@piko.com.au>
2021-08-05 07:31:32 +1000

    added

---
+++ resetResolv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+if [ "$(id -u)" != "0" ] 
+then
+    echo "Only root can run this."
+    exit 2
+fi
+
+RESOLV=$(grep nameserver /etc/resolv.conf | gawk '{print $2}'| cut -f1 -d.)
+NET=$(hostname -I | cut -f1-3 -d.)
+
+if [ $NET == "192.168.0" ] && [ $RESOLV == "127" ]
+then
+        echo "nameserver 192.168.0.1" > /etc/resolv.conf
+fi
