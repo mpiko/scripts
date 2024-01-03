a23c1b26771c9f4a7b733bfa420f7a4642b18b1e
Your Name <youremail@example.com>
2024-01-04 06:47:47 +1100

    fixed bugs

--- disableResolve.sh
+++ disableResolve.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+if [ $(id -u) -ne 0 ]
+then
+    sudo $0
+else
 
-sudo systemctl stop systemd-resolved
-sudo systemctl disable systemd-resolved
+systemctl stop systemd-resolved
+systemctl disable systemd-resolved
 
-sudo cat >/etc/resolv.conf<<EOF
+rm /etc/resolv.conf
+
+cat > /etc/resolv.conf<<EOF
 nameserver 10.10.10.100
 options edns0 trust-ad
 search cyder.com.au wombat.local
 EOF
+fi
